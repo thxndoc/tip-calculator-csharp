9412e68e2db9cbf128fdbf4d450f3081bcafcd05
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-04-08 14:14:06 +0200

    add welcome message and bill amount prompt

--- TipCalculator/TipCalculator/Program.cs
+++ TipCalculator/TipCalculator/Program.cs
@@ -8,8 +8,24 @@ namespace TipCalculator
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            int tipPercentage;
+            int numOfPeopleSplittingBill;
+
+            Console.WriteLine("Welcome to the tip calculator! \n");
+
+            Console.WriteLine("Enter the bill amount: ");
+            decimal billAmount = decimal.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the tip percentage (e.g., 10 for 10%):");
+            //tipPercentage = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("How many people are splitting the bill?");
+            //numOfPeopleSplittingBill = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
         }
     }
 }
