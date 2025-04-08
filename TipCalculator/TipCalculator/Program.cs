8d29ff033f66a5f65c2100b83263c3c032455c23
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-04-08 15:37:11 +0200

    add line breaks

--- TipCalculator/TipCalculator/Program.cs
+++ TipCalculator/TipCalculator/Program.cs
@@ -22,12 +22,15 @@ namespace TipCalculator
 
             Console.WriteLine("Enter the bill amount (e,g., 200,95):");
             billAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
 
             Console.WriteLine("Enter the tip percentage (e.g., 10 for 10%)");
             tipPercentage = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
             Console.WriteLine("How many people are splitting the bill?");
             numOfPeopleSplittingBill = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
             tipAmount = billAmount * (tipPercentage / 100m);
             Console.WriteLine($"Tip amount: R{tipAmount:F2}");
