3e585bdc8f15f53de3fa650699d6284c8ee29321
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-04-08 14:56:39 +0200

    prompt user to enter number of people splitting the bill

--- TipCalculator/TipCalculator/Program.cs
+++ TipCalculator/TipCalculator/Program.cs
@@ -13,14 +13,18 @@ namespace TipCalculator
         {
             decimal billAmount;
             int tipPercentage;
+            int numOfPeopleSplittingBill;
 
             Console.WriteLine("Welcome to the tip calculator! \n");
 
-            Console.WriteLine("Enter the bill amount (e,g., 200,95):\n");
+            Console.WriteLine("Enter the bill amount (e,g., 200,95):");
             billAmount = decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter the tip percentage (e.g., 10 for 10%)");
             tipPercentage = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many people are splitting the bill?");
+            numOfPeopleSplittingBill = int.Parse(Console.ReadLine());
 
             Console.ReadKey();
         }
